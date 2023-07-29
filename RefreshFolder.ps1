a4c87f8824837370f3bfed7595be876d7d9214df
rulasg <rulasg@outlook.com>
2023-07-29 13:06:18 +0200

    improve refreshfolder.ps1

--- RefreshFolder.ps1
+++ RefreshFolder.ps1
@@ -11,31 +11,58 @@ param(
 )
 
 function backupfile($Path){
+    
+    #check that $Path is not null
+    if(!$Path){ Write-Error "Path is null" ; return }
+
+    # CHeck if that does not exist
+    if(!(Test-Path -Path $Path)){ Write-Error "Path does not exist" ; return }
+
+    # check if 
+
+    $file = Get-Item -Path $Path
+
     # create backup folder if does not exist
     if(!(Test-Path -Path backup)){
         New-Item -Path backup -ItemType Directory
     }
 
-    #check that $Path is not null
-    if(!$Path){
-        Write-Error "Path is null"
-        return
-    }
-
     $id = 0
     do {
-        $destination = "backup" | Join-Path -ChildPath ("{0}({1}).txt" -f $path.basename,$id++)
+        $id++
+        $destination = "backup" | Join-Path -ChildPath ("{0}({1}){2}" -f $file.BaseName,$id,$file.Extension)
         Write-Verbose "Destination: $destination"
     } until ( ! ($destination | Test-Path))
 
-    Move-Item -Path $path -Destination $destination
+    # check if both files are the same
+    $previouse = "backup" | Join-Path -ChildPath ("{0}({1}){2}" -f $file.BaseName,($id-1),$file.Extension)
+
+    if(AreEqual -source $file -destination $previouse){
+        Write-Verbose "Files are the same"
+        return
+    }
+
+    # Backup $file
+    $file | Copy-Item -Destination $destination
+}
+
+function AreEqual($source,$destination){
+
+    # check if both files exist
+    if(!(Test-Path -Path $source)){ return $false}
+    if(!(Test-Path -Path $destination)){ return $false }
+
+    $sourceHash = Get-FileHash -Path $source
+    $destinationHash = Get-FileHash -Path $destination
+    return $sourceHash.Hash -eq $destinationHash.Hash
 }
 
 $scriptFiles = Get-ChildItem -Path *.scpt
 
 foreach($file in $scriptFiles){
-    backupfile -Path $file
-    osadecompile $file.FullName | Out-File -FilePath $file.FullName
+    $codeFile = $file.Directory | Join-Path -ChildPath ($file.BaseName + ".txt")
+    backupfile -Path $codeFile
+    osadecompile $file.FullName | Out-File -FilePath $codeFile
 }
 
 
